ad76f10063bcbbf8d447653db4b942b3d586e12d
Moritz Wilhelmy <moritz+git@wzff.de>
2016-08-14 01:21:33 +0200

    And that concludes the saturday night

--- init.lisp
+++ init.lisp
@@ -124,43 +124,17 @@ Load a file that re-defines swank and then calls it."
 ;; Clear rules
 (clear-window-placement-rules)
 
-;; Last rule to match takes precedence!
-;; TIP: if the argument to :title or :role begins with an ellipsis, a substring
-;; match is performed.
-;; TIP: if the :create flag is set then a missing group will be created and
-;; restored from *data-dir*/create file.
-;; TIP: if the :restore flag is set then group dump is restored even for an
-;; existing group using *data-dir*/restore file.
-;(define-frame-preference "Default"
-;  ;; frame raise lock (lock AND raise == jumpto)
-;  (0 t nil :class "Konqueror" :role "...konqueror-mainwindow")
-;  (1 t nil :class "XTerm"))
-;
-;(define-frame-preference "Ardour"
-;  (0 t   t   :instance "ardour_editor" :type :normal)
-;  (0 t   t   :title "Ardour - Session Control")
-;  (0 nil nil :class "XTerm")
-;  (1 t   nil :type :normal)
-;  (1 t   t   :instance "ardour_mixer")
-;  (2 t   t   :instance "jvmetro")
-;  (1 t   t   :instance "qjackctl")
-;  (3 t   t   :instance "qjackctl" :role "qjackctlMainForm"))
-;
-;(define-frame-preference "Shareland"
-;  (0 t   nil :class "XTerm")
-;  (1 nil t   :class "aMule"))
-;
-;(define-frame-preference "Emacs"
-;  (1 t t :restore "emacs-editing-dump" :title "...xdvi")
-;  (0 t t :create "emacs-dump" :class "Emacs"))
+(setq *data-dir* #p"~/.stumpwm.d/data/")
 
-(gnewbg "Pidgin")
-(gnewbg "WWW")
+(mapcar #'gnewbg '("Pidgin" "Web" "Code" "Extra"))
 
 (define-frame-preference "Pidgin"
-  (2 t t :create "pidgin.dump" :class "Pidgin" :instance "Pidgin" :title nil :role nil)
-  (0 t t :create "pidgin.dump" :class "Pidgin" :instance "Pidgin" :title nil :role "buddy_list")
-  (1 t t :create "pidgin.dump" :class "Pidgin" :instance "Pidgin" :title nil :role "conversation"))
+  (2 t t :create "pidgin" :restore "pidgin" :class "Pidgin" :instance "Pidgin" :title nil :role nil)
+  (0 t t :create "pidgin" :restore "pidgin" :class "Pidgin" :instance "Pidgin" :title nil :role "buddy_list")
+  (1 t t :create "pidgin" :restore "pidgin" :class "Pidgin" :instance "Pidgin" :title nil :role "conversation"))
 
-(define-frame-preference "WWW"
-  (0 t t :create "www.dump" :class "Firefox"))
+(define-frame-preference "Web"
+    (0 t t :create "web" :restore "web" :class "Firefox"))
+
+(define-frame-preference "Code"
+    (0 t t :create "code" :restore "code" :class "Emacs"))
